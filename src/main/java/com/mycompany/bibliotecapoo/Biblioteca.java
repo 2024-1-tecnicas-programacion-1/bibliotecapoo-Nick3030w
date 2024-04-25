9e81d86ea4521e5849ae9b065bbdb93784a33f41
Nick3030w <niconianessu@gmail.com>
2024-04-25 08:56:57 -0500

    Se creo la clase libro con todos sus metodos y complejidades, y algunos metodos de la clase biblioteca, falta completar los metodos de la clase biblioteca y terminar el Metodo principal.

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -1,5 +1,30 @@
 package com.mycompany.bibliotecapoo;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Biblioteca {
-    // TODO: Aquí va tu código
+    private LinkedList<String> libros;
+    Scanner lector = new Scanner(System.in);
+    
+    
+    public void registrarLibro(){
+        libros.add(Libro);
+    }
+    
+    public String buscarLibro(){
+        String palabraBusqueda = lector.next();
+        return libros.get(palabraBusqueda);
+    }
+    
+    void mostrarLibros(){
+        for(int i = 0; i < libros.size(); i++) {
+            System.out.println(libros.get(i));
+        }
+    }
+    void mostrarLibrosNoLeidos(){
+        if(Libro.marcarLeido = false){
+            
+        }
+    }
+
 }
